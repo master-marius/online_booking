84af61e88d674b15f17fd8d14c47b534095c7c93
master-marius <mastermicco@gmail.com>
2016-05-27 10:14:12 +0000

    set current_member

--- online_booking_test/app/controllers/dashboard_controller.rb
+++ online_booking_test/app/controllers/dashboard_controller.rb
@@ -1,5 +1,13 @@
 class DashboardController < ApplicationController
+  before_filter :current_member
+
   def index
     
+  end
+
+  protected
+
+  def current_member
+    @current_member = warden.authenticate(:scope => :member)
   end
 end
