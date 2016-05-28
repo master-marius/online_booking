3a35d6c749921d34521c962e230b56a3c8c5c271
master-marius <mastermicco@gmail.com>
2016-05-28 22:52:15 +0800

    change time format for schedule

--- online_booking_test/app/serializers/schedule_serializer.rb
+++ online_booking_test/app/serializers/schedule_serializer.rb
@@ -2,7 +2,7 @@ class ScheduleSerializer < ActiveModel::Serializer
   attributes :id, :teacher_id, :date, :time
 
   def time
-    object.time.strftime("%H:%M").to_s
+    object.time.strftime("%I:%M %P").to_s 
   end
   
 end
